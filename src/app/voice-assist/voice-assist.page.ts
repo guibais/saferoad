790d0a553636f3d2ad75604d45f298ffe5a45380
Gui Bais <baissousa.guilherme@gmail.com>
2020-06-14 08:29:00 -0300

    feat: android

--- src/app/voice-assist/voice-assist.page.ts
+++ src/app/voice-assist/voice-assist.page.ts
@@ -7,16 +7,48 @@ import { SpeechRecognition } from '@ionic-native/speech-recognition/ngx';
   styleUrls: ['./voice-assist.page.scss'],
 })
 export class VoiceAssistPage implements OnInit {
-  private isRecording = false;
+  isRecording = false;
+  messages = [
+    {
+      text: 'Onde eu posso parar meu caminhão?',
+      me: true,
+    },
+    {
+      text: 'Olá João! Você pode parar no posto de gasolina mais próximo.',
+      me: false,
+    },
+    {
+      text: 'Gostaria de ver as informações?',
+      me: false,
+    },
+  ];
   constructor(private speechRecognition: SpeechRecognition) {}
 
   ngOnInit() {}
 
   async toggleRecording() {
-    this.isRecording = !this.isRecording;
-    if (this.isRecording) {
-      const hasPermission = await this.speechRecognition.hasPermission();
-      console.log(hasPermission);
+    const hasPermission = await this.speechRecognition.hasPermission();
+    if (hasPermission) {
+      this.isRecording = true;
+      let options = {
+        language: 'pt-BR',
+        matches: 1,
+        prompt: 'Ouvindo...',
+        showPopup: true,
+      };
+      this.speechRecognition.startListening(options).subscribe(
+        (matches: string[]) => {
+          console.log(matches);
+          this.isRecording = false;
+        },
+        (onerror) => {
+          console.log('error:', onerror);
+          this.isRecording = false;
+        }
+      );
+    } else {
+      const askPermission = await this.speechRecognition.requestPermission();
+      this.toggleRecording();
     }
   }
 }
